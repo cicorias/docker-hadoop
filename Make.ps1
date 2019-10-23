d740ac5c5b7557ceef53ead76ee5729cab5b1c3b
cicorias <github@cicoria.com>
2019-10-22 21:58:50 -0400

    fix updates

--- Make.ps1
+++ Make.ps1
@@ -1,8 +1,9 @@
 $DOCKER_NETWORK = "docker-hadoop_default"
 $ENV_FILE = "hadoop.env"
-$current_branch ="3.1.2" # $(shell git rev-parse --abbrev-ref HEAD)
+$current_branch ="3.2.1" # $(shell git rev-parse --abbrev-ref HEAD)
 function build() {
-  docker build -t cicorias/hadoop-base:$current_branch ./base
+	docker build -t cicorias/hadoop-base:$current_branch ./base
+	docker tag cicorias/hadoop-base:$current_branch cicorias/hadoop-base:latest
 	docker build -t cicorias/hadoop-namenode:$current_branch ./namenode
 	docker build -t cicorias/hadoop-datanode:$current_branch ./datanode
 	docker build -t cicorias/hadoop-resourcemanager:$current_branch ./resourcemanager
@@ -15,7 +16,7 @@ function wordcount() {
   wordcount:
 	docker build -t hadoop-wordcount ./submit
 	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} cicorias/hadoop-base:$current_branch hdfs dfs -mkdir -p /input/
-	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} cicorias/hadoop-base:$current_branch hdfs dfs -copyFromLocal /opt/hadoop-3.1.2/README.txt /input/
+	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} cicorias/hadoop-base:$current_branch hdfs dfs -copyFromLocal /opt/hadoop-3.2.1/README.txt /input/
 	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} hadoop-wordcount
 	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} cicorias/hadoop-base:$current_branch hdfs dfs -cat /output/*
 	docker run --network ${DOCKER_NETWORK} --env-file ${ENV_FILE} cicorias/hadoop-base:$current_branch hdfs dfs -rm -r /output
